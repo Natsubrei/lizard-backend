33f4efbdf8a8b12a7bd19560e9ca1a32e9819aba
Natsubrei <natsunobourei@163.com>
2025-04-01 19:04:59 +0800

    项目初始化

---
+++ src/main/java/com/lizard/lizardbackend/LizardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lizard.lizardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LizardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lizard/lizardbackend/LizardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizard.lizardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LizardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
